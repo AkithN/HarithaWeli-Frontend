f1ffbcebd250a438070572c49b35097e3fd3fbd1
OreshaW <MAHNDSE231F-016@student.nibm.lk>
2025-02-04 23:29:13 +0530

    create Agent dashboard

---
+++ src/Pages/AgentDashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AdminNavbar from '../Components/AdminNavbar';
+import AdminSideBar from '../Components/AdminSideBar';
+import AdminDash from '../Containers/AdminDash';
+
+const AgentDashboard = () => {
+return (
+    <div className="flex mt-20">
+      <AdminSideBar />
+
+       <div className="flex flex-col w-full">
+        <AdminNavbar />
+
+        <div className="ml-0 mt-0 ">
+          <AdminDash />
+        </div>
+     </div>
+   </div>
+  );
+};
+export default AgentDashboard;
